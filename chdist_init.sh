1719297b9685d311faa51b6f7daf50f20cda4cfd
sebma <sebma@users.noreply.github.com>
2024-12-07 16:27:42 +0100

    Updated chdist_init.sh

--- chdist_init.sh
+++ chdist_init.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-for distrib in precise trusty noble jammy bionic xenial focal
-do
+set -u
+for distrib in trusty xenial bionic focal jammy noble;do
 	if ! [ -d $distrib ] && ! [ -L $distrib ];then
 		echo "= chdist create $distrib ..."
 		chdist create $distrib
+#		distribNumber=""
+#		ln -s $distrib $distribNumber
 	fi
 done
