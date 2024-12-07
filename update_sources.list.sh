6623657bc241c82f521426dba6f7b86f5f2bfaa4
sebma <sebma@users.noreply.github.com>
2024-12-07 16:14:20 +0100

    Updated update_sources.list.sh

---
+++ update_sources.list.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+find -maxdepth 1 -type d | egrep -v '^.$|^./.git$' | sed 's|^./||' | while read distrib;do
+	cat /etc/apt/sources.list | sed "s/$(\lsb_release -sc)/$distrib/" | tee ~/.chdist/$distrib/etc/apt/sources.list >/dev/null
+done
