b896ada6217b604c7be6b7afabe19329b2e5d31d
sebma <sebma@users.noreply.github.com>
2024-12-09 20:58:07 +0100

    Updated chdist_init.sh

--- chdist_init.sh
+++ chdist_init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -u
-for distribNumber in $(LC_NUMERIC=C seq 14.04 2 24);do
+for distribNumber in $(LC_NUMERIC=C seq 14.04 2 24.04);do
 	if ! [ -d $distribNumber ] && ! [ -L $distribNumber ];then
 		echo "= chdist create $distribNumber ..."
 		chdist create $distribNumber
