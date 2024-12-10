907ecb8620c15c38cbb56f2556c2f2b5cae6dd08
sebma <sebma@users.noreply.github.com>
2024-12-10 18:26:20 +0100

    Update chdist_init.sh

--- chdist_init.sh
+++ chdist_init.sh
@@ -29,3 +29,6 @@ proxyFile=$(grep Proxy /etc/apt/apt.conf.d/* -m1 -l)
 if [ -n "$proxyFile" ] && [ -s "$proxyFile" ];then
 	for distribNumber in $(LC_NUMERIC=C seq 14.04 2 24);do mkdir -p ~/.chdist/$distribNumber/etc/apt/apt.conf.d/ && cp -puv $proxyFile ~/.chdist/$distribNumber/etc/apt/apt.conf.d/;done
 fi
+
+#Creating $HOME/.chdist/%s/etc/apt/apt.conf.d/ dirs cf. http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=578446
+mkdir -v $(printf "$HOME/.chdist/%s/etc/apt/apt.conf.d/ " $(LC_NUMERIC=C seq 14.04 2 24.04))
